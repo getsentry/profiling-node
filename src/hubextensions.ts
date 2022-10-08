6d1974fdd5b193cc5b5a3b1d50b2f52a0ed6feb5
JonasBa <jonas@badalic.com>
2022-10-08 10:47:48 -0400

    feat(skd): add better messaging when we cannot patch the lib

--- src/hubextensions.ts
+++ src/hubextensions.ts
@@ -77,15 +77,22 @@ function _addProfilingExtensionMethods(): void {
   }
   carrier.__SENTRY__.extensions = carrier.__SENTRY__.extensions || {};
 
-  if (carrier.__SENTRY__.extensions['startTransaction']) {
+  if (!carrier.__SENTRY__.extensions['startTransaction']) {
     if (isDebugBuild()) {
-      logger.log('[Profiling] startTransaction exists, patching it with profiling.');
+      logger.log(
+        '[Profiling] startTransaction does not exists, profiling will not work. Make sure you import @sentry/tracing package before @sentry/profiling-node as import order matters.'
+      );
     }
-    carrier.__SENTRY__.extensions['startTransaction'] = __PRIVATE__wrapStartTransactionWithProfiling(
-      // This is already patched by sentry/tracing, we are going to re-patch it...
-      carrier.__SENTRY__.extensions['startTransaction'] as StartTransaction
-    );
+    return;
   }
+
+  if (isDebugBuild()) {
+    logger.log('[Profiling] startTransaction exists, patching it with profiling functionality...');
+  }
+  carrier.__SENTRY__.extensions['startTransaction'] = __PRIVATE__wrapStartTransactionWithProfiling(
+    // This is already patched by sentry/tracing, we are going to re-patch it...
+    carrier.__SENTRY__.extensions['startTransaction'] as StartTransaction
+  );
 }
 
 /**
