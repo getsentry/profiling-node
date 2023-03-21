e953e8a9db34ba3808e536876888b4f47d29524e
Kev <6111995+k-fish@users.noreply.github.com>
2023-03-21 11:59:27 -0400

    Add MAX_PROFILE_DURATION_MS import

--- src/integration.ts
+++ src/integration.ts
@@ -3,7 +3,7 @@ import type { Integration, EventProcessor, Hub, Event, Transaction } from '@sent
 
 import { logger } from '@sentry/utils';
 import { isDebugBuild } from './env';
-import { addProfilingExtensionMethods, maybeProfileTransaction, stopTransactionProfile } from './hubextensions';
+import { addProfilingExtensionMethods, maybeProfileTransaction, stopTransactionProfile, MAX_PROFILE_DURATION_MS } from './hubextensions';
 
 import type { RawThreadCpuProfile } from './cpu_profiler';
 import { Profile, addProfilesToEnvelope } from './utils';
