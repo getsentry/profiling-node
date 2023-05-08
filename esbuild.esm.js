0b3176f24f735f43a0029381c1a6fbf2a4e31a9d
JonasBa <jonas@badalic.com>
2023-05-08 19:33:13 -0400

    feat(profiling): enable running esm directly

--- esbuild.esm.js
+++ esbuild.esm.js
@@ -2,9 +2,19 @@
 require('esbuild').build({
   platform: 'node',
   entryPoints: ['./src/index.ts'],
-  outfile: './lib/esm/index.js',
+  outfile: './lib/esm/index.mjs',
   format: 'esm',
   target: 'esnext',
   bundle: true,
-  tsconfig: './tsconfig.esm.json'
+  tsconfig: './tsconfig.esm.json',
+  banner: {
+    js: `
+  import {dirname as topLevelAliasedDirname} from 'path';
+    import { fileURLToPath } from 'url';
+    import { createRequire as topLevelCreateRequire } from 'module';
+    const require = topLevelCreateRequire(import.meta.url);
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = topLevelAliasedDirname(__filename);
+  //   `
+  }
 });
--- scripts/esm.mod.js
+++ scripts/esm.mod.js
@@ -4,12 +4,12 @@ let contents = fs.readFileSync('./src/cpu_profiler.ts', 'utf8');
 // Convert to CJS
 contents = contents.replace(
   /\/\/\s+__START__REPLACE__DIRNAME__((.|\r|\n)*)__END__REPLACE__DIRNAME__/gm,
-  `// __START__REPLACE__DIRNAME__\nimport { dirname } from 'path';\nimport { fileURLToPath } from 'url';\nconst _dirname = dirname(fileURLToPath(import.meta.url));\n// __END__REPLACE__DIRNAME__`
+  `// __START__REPLACE__DIRNAME__\nimport { dirname as aliasedDirname } from 'path';\nimport { fileURLToPath as aliasedFileUrlToPath } from 'url';\nconst _dirname = aliasedDirname(aliasedFileUrlToPath(import.meta.url));\n// __END__REPLACE__DIRNAME__`
 );
 
 contents = contents.replace(
   /\/\/\s+__START__REPLACE__REQUIRE__((.|\r|\n)*)__END__REPLACE__REQUIRE__/gm,
-  `// __START__REPLACE__REQUIRE__\nimport { createRequire } from 'module';\nconst _require = createRequire(import.meta.url);\n// __END__REPLACE__REQUIRE__`
+  `// __START__REPLACE__REQUIRE__\nimport { createRequire as aliasedCreateRequire } from 'module';\nconst _require = aliasedCreateRequire(import.meta.url);\n// __END__REPLACE__REQUIRE__`
 );
 
 fs.writeFileSync('./src/cpu_profiler.ts', contents, 'utf8');
