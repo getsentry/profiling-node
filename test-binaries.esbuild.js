8c11c7f6782625fe87aaf84b968a553a299a8cf6
JonasBa <jonas@badalic.com>
2023-05-09 14:14:28 -0400

    ci: add bundle compile test

--- test-binaries.esbuild.js
+++ test-binaries.esbuild.js
@@ -3,7 +3,7 @@ import esbuild from 'esbuild';
 esbuild.build({
   platform: 'node',
   entryPoints: ['./test-binaries.entry.js'],
-  outfile: './test-binaries.js',
+  outfile: './build-test/test-binaries.js',
   format: 'cjs',
   bundle: true,
   tsconfig: './tsconfig.cjs.json',
