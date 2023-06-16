07f2fd4dc5db9896a310b6ce3e5a305bc8f0a45a
JonasBa <jonas@badalic.com>
2023-06-16 09:48:56 -0400

    build: improve recompile error handling

--- scripts/check-build.mjs
+++ scripts/check-build.mjs
@@ -12,12 +12,11 @@ function recompileFromSource() {
     cp.execSync(`npm run build:configure`, { env: process.env });
     cp.execSync(`npm run build:bindings`, { env: process.env });
     cp.execSync('node scripts/copy-target.mjs', { env: process.env });
-    return true;
   } catch (e) {
     console.error(
       '@sentry/profiling-node: Failed to build from source, please report this a bug at https://github.com/getsentry/profiling-node/issues/new?assignees=&labels=Type%3A+Bug&template=bug.yml'
     );
-    return false;
+    return e;
   }
 }
 
@@ -28,36 +27,28 @@ try {
     console.log('@sentry/profiling-node: Precompiled binary found, skipping build from source.');
   } else {
     console.log('@sentry/profiling-node: Precompiled binary not found, attempting to compile...');
-    const success = recompileFromSource();
-    if (success) {
-      process.exit(0);
-    }
+    const error = recompileFromSource();
+    if (error) throw error;
   }
 } catch (e) {
   console.log('@sentry/profiling-node: Precompiled binary found but failed loading');
 
   // Check for node version missmatch
   if (/was compiled against a different Node.js/.test(e.message)) {
-    const success = recompileFromSource();
-    if (success) {
-      process.exit(0);
-    }
+    const error = recompileFromSource();
+    if (error) throw error;
   }
 
   // Not sure if this could even happen, but just in case it somehow does,
   // we can provide a better experience than just crashing with cannot find module message.
   if (/Cannot find module/.test(e.message)) {
-    const success = recompileFromSource();
-    if (success) {
-      process.exit(0);
-    }
+    const error = recompileFromSource();
+    if (error) throw error;
   }
 
   if (/cannot open shared object file/.test(e.message)) {
-    const success = recompileFromSource();
-    if (success) {
-      process.exit(0);
-    }
+    const error = recompileFromSource();
+    if (error) throw error;
   }
 
   // re-throw so we dont end up swallowing errors
