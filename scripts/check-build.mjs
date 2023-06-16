154c04162b4bc9e8a3787fbc306a3429c9753161
Jonas <jonas.badalic@sentry.io>
2023-06-16 11:15:16 -0400

    build: stop error handling and just propagate all errors (#167)
    * build: log target
    
    * build: handle missing error
    
    * build: more accurate logging

--- scripts/check-build.mjs
+++ scripts/check-build.mjs
@@ -8,38 +8,39 @@ const require = createRequire(import.meta.url);
 
 function recompileFromSource() {
   console.log('@sentry/profiling-node: Compiling from source...');
-  cp.execSync(`npm run build:configure`, { env: process.env });
-  cp.execSync(`npm run build:bindings`, { env: process.env });
-  cp.execSync('node scripts/copy-target.mjs', { env: process.env });
+  cp.spawnSync(`npm run build:configure`, { env: process.env });
+  console.log('@sentry/profiling-node: Configured gyp');
+  cp.spawnSync(`npm run build:bindings`, { env: process.env });
+  console.log('@sentry/profiling-node: Built binary');
+  cp.spawnSync('node scripts/copy-target.mjs', { env: process.env });
+  console.log('@sentry/profiling-node: Moved binary');
 }
 
-try {
-  if (existsSync(target)) {
-    console.log('@sentry/profiling-node: Precompiled binary found, attempting to load...');
+if (existsSync(target)) {
+  try {
+    console.log(`@sentry/profiling-node: Precompiled binary found, attempting to load ${target}`);
     require(target);
     console.log('@sentry/profiling-node: Precompiled binary found, skipping build from source.');
-  } else {
-    console.log('@sentry/profiling-node: Precompiled binary not found, attempting to compile...');
-    recompileFromSource();
-  }
-} catch (e) {
-  console.log('@sentry/profiling-node: Precompiled binary found but failed loading');
+  } catch (e) {
+    console.log('@sentry/profiling-node: Precompiled binary found but failed loading');
+    if (/was compiled against a different Node.js/.test(e.message)) {
+      recompileFromSource();
+    }
 
-  // Check for node version missmatch
-  if (/was compiled against a different Node.js/.test(e.message)) {
-    recompileFromSource();
-  }
+    // Not sure if this could even happen, but just in case it somehow does,
+    // we can provide a better experience than just crashing with cannot find module message.
+    if (/Cannot find module/.test(e.message)) {
+      recompileFromSource();
+    }
 
-  // Not sure if this could even happen, but just in case it somehow does,
-  // we can provide a better experience than just crashing with cannot find module message.
-  if (/Cannot find module/.test(e.message)) {
-    recompileFromSource();
-  }
+    if (/cannot open shared object file/.test(e.message)) {
+      recompileFromSource();
+    }
 
-  if (/cannot open shared object file/.test(e.message)) {
-    recompileFromSource();
+    // re-throw so we dont end up swallowing errors
+    throw e;
   }
-
-  // re-throw so we dont end up swallowing errors
-  throw e;
+} else {
+  console.log('@sentry/profiling-node: No precompiled binary found');
+  recompileFromSource();
 }
--- scripts/copy-target.mjs
+++ scripts/copy-target.mjs
@@ -1,18 +1,24 @@
-import fs from 'fs';
+import { existsSync, mkdirSync, renameSync } from 'fs';
 import path from 'path';
 import { fileURLToPath, URL } from 'url';
+import { exit } from 'process';
 
 import { getModuleName } from './binaries.mjs';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 const lib = path.resolve(__dirname, '..', 'lib');
 
-if (!fs.existsSync(lib)) {
-  fs.mkdirSync(lib);
+if (!existsSync(lib)) {
+  mkdirSync(lib);
 }
 
 const source = path.join(__dirname, '..', 'build', 'Release', 'sentry_cpu_profiler.node');
 const target = path.join(__dirname, '..', 'lib', getModuleName());
 
-console.log('Renaming', source, 'to', target);
-fs.renameSync(source, target);
+if (!existsSync(source)) {
+  console.log('Source file does not exist:', source);
+  exit(1);
+} else {
+  console.log('Renaming', source, 'to', target);
+  renameSync(source, target);
+}
