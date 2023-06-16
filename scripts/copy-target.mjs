45d4d8047bee3c80f55358215e76db8bd8b629f6
JonasBa <jonas@badalic.com>
2023-06-16 10:17:00 -0400

    build: more accurate logging

--- scripts/copy-target.mjs
+++ scripts/copy-target.mjs
@@ -1,4 +1,4 @@
-import fs, { existsSync } from 'fs';
+import { existsSync, mkdirSync, renameSync } from 'fs';
 import path from 'path';
 import { fileURLToPath, URL } from 'url';
 import { exit } from 'process';
@@ -8,8 +8,8 @@ import { getModuleName } from './binaries.mjs';
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 const lib = path.resolve(__dirname, '..', 'lib');
 
-if (!fs.existsSync(lib)) {
-  fs.mkdirSync(lib);
+if (!existsSync(lib)) {
+  mkdirSync(lib);
 }
 
 const source = path.join(__dirname, '..', 'build', 'Release', 'sentry_cpu_profiler.node');
@@ -20,5 +20,5 @@ if (!existsSync(source)) {
   exit(1);
 } else {
   console.log('Renaming', source, 'to', target);
-  fs.renameSync(source, target);
+  renameSync(source, target);
 }
