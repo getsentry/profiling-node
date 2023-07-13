fd4a061e41e68f05222d4ce75d187bc54329a603
JonasBa <jonas@badalic.com>
2023-07-13 15:30:27 -0400

    fix:rebuild if we have a dlopen error

--- scripts/check-build.mjs
+++ scripts/check-build.mjs
@@ -19,23 +19,7 @@ if (existsSync(target)) {
     require(target);
     console.log('@sentry/profiling-node: Precompiled binary found, skipping build from source.');
   } catch (e) {
-    console.log('@sentry/profiling-node: Precompiled binary found but failed loading');
-    if (/was compiled against a different Node.js/.test(e.message)) {
-      recompileFromSource();
-    }
-
-    // Not sure if this could even happen, but just in case it somehow does,
-    // we can provide a better experience than just crashing with cannot find module message.
-    if (/Cannot find module/.test(e.message)) {
-      recompileFromSource();
-    }
-
-    if (/cannot open shared object file/.test(e.message)) {
-      recompileFromSource();
-    }
-
-    // re-throw so we dont end up swallowing errors
-    throw e;
+    recompileFromSource();
   }
 } else {
   console.log('@sentry/profiling-node: No precompiled binary found');
