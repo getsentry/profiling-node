18b3ab69402ec36db4f2237c934efa39c03f0386
JonasBa <jonas@badalic.com>
2023-07-14 14:42:02 -0400

    ci: add webpack and rollup

--- demo_app/index.js
+++ demo_app/index.js
@@ -1,5 +1,6 @@
 const lib = require('@sentry/node');
 const profiling = require('./../lib/index.js');
+const process = require('process');
 
 lib.init({
   dsn: 'https://03fdc938a5f3431ea023c381b759669c@o1.ingest.sentry.io/4505528192335872',
@@ -8,7 +9,7 @@ lib.init({
   profilesSampleRate: 1
 });
 
-const transaction = lib.startTransaction({ name: 'smoke-test-application-txn' });
+const transaction = lib.startTransaction({ name: `${process.env['BUNDLER']}-application-build` });
 
 function sleep(time) {
   const stop = new Date().getTime();
