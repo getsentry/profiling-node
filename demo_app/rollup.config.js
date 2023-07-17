07d7ccd1e546aaeed4a33b14305e9f8968ea9fd2
JonasBa <jonas@badalic.com>
2023-07-17 12:07:43 -0400

    link in ci

--- demo_app/rollup.config.js
+++ demo_app/rollup.config.js
@@ -17,12 +17,12 @@ module.exports = {
     commonjs({
       strictRequires: true
     }),
-    // rollupNativePlugin({
-    //   // Where we want to physically put the extracted .node files
-    //   copyTo: path.resolve(__dirname, './dist/rollup'),
+    rollupNativePlugin({
+      // Where we want to physically put the extracted .node files
+      copyTo: path.resolve(__dirname, './dist/rollup'),
 
-    //   // Path to the same folder, relative to the output bundle js
-    //   destDir: path.resolve(__dirname, './dist/rollup'),
-    // }),
+      // Path to the same folder, relative to the output bundle js
+      destDir: path.resolve(__dirname, './dist/rollup'),
+    }),
   ]
 };
