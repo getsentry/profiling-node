d686fba644f43c76ad11f18c73d9e945dfccfdf5
JonasBa <jonas@badalic.com>
2024-01-04 09:34:20 -0500

    ref: build ts

--- lib/integration.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Integration, EventProcessor, Hub, Event } from '@sentry/types';
-export declare class ProfilingIntegration implements Integration {
-    name: string;
-    getCurrentHub?: () => Hub;
-    setupOnce(addGlobalEventProcessor: (callback: EventProcessor) => void, getCurrentHub: () => Hub): void;
-    handleGlobalEvent(event: Event): Event;
-}
-//# sourceMappingURL=integration.d.ts.map
